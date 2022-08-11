2258c72e6093b9afe8ed3d4faeddbcc9d7dd89e5
HaissamHammoudFawaz <haissam_fawaz_4@hotmail.com>
2022-08-10 23:23:49 -0300

    fix route name

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as express from 'express';
+import express from 'express';
 import prof from './routes/dashprof';
 import alunos from './routes/alunos';
 
--- src/routes/alunos.ts
+++ src/routes/alunos.ts
@@ -4,16 +4,16 @@ import  * as alunos from '../controllers/aluno'
 const router = Router()
 
 
-router.get('/aluno/vagasmonitoria', alunos.getVagasMonitoria)
-router.get('/aluno/monitor/minhasmonitorias', alunos.getMinhasMonitorias)
-router.get('/aluno/monitor/agendamento/monitoria', alunos.getAgendamentoMonitoria)
-router.put('/aluno/monitor/agendamento/solicitacao/finalizar', alunos.finalizarSolicitacaoAgentamento) //PUT
-router.delete('/aluno/monitor/agendamento/remover', alunos.removerAgendamento) // DELETE
-router.get('/aluno/monitor/agendamentos', alunos.getAgendamentos)
-router.get('/aluno/perfil', alunos.getPerfil)
-router.get('/aluno/monitorias', alunos.getMonitorias)
-router.get('/aluno/monitoria', alunos.getMonitoria)
-router.post('/aluno/agendar/monitoria', alunos.agendarMonitoria) //post
-router.post('/aluno/solicitar/vaga/monitoria', alunos.solicitarVagaMonitoria) //post
+router.get('/vagasmonitoria', alunos.getVagasMonitoria)
+router.get('/monitor/minhasmonitorias', alunos.getMinhasMonitorias)
+router.get('/monitor/agendamento/monitoria', alunos.getAgendamentoMonitoria)
+router.put('/monitor/agendamento/solicitacao/finalizar', alunos.finalizarSolicitacaoAgentamento) //PUT
+router.delete('/monitor/agendamento/remover', alunos.removerAgendamento) // DELETE
+router.get('/monitor/agendamentos', alunos.getAgendamentos)
+router.get('/perfil', alunos.getPerfil)
+router.get('/monitorias', alunos.getMonitorias)
+router.get('/monitoria', alunos.getMonitoria)
+router.post('/agendar/monitoria', alunos.agendarMonitoria) //post
+router.post('/solicitar/vaga/monitoria', alunos.solicitarVagaMonitoria) //post
 
 export default router;

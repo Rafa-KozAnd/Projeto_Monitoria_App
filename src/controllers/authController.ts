1943fc991f478261e1dc22c3926ddd0a6a02d06e
haissam.fawaz <haissam.fawaz@trocosimples.combr>
2022-09-08 02:22:22 +0000

    Add authorization service to authentiation route

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,28 +2,44 @@ import { compare } from "bcryptjs";
 import { RequestHandler } from "express"
 import { v4 as uuidv4 } from 'uuid';
 import { client } from "../../prisma/client";
+import {Authenticator} from "../services/Authenticator"
+import {Aluno, Colaborador} from "../services/Aluno"
 
-export const alunoLogin: RequestHandler  = (req, res) => {
-  res.status(200).json({user:{ role: 'Professor', nome: 'Joao Pedro'}})
+
+const alunoLogin: RequestHandler = async (req, res) => {
+  const {
+      matricula,
+      senha
+  } = req.body;
+  console.log("entuenticando");
+  const aluno = new Aluno(matricula, senha)
+  const validate = await Authenticator.authenticateAluno(aluno)
+  if (validate["valid"] != true )
+  {
+      res.status(403).send(validate);
+      return false
+  }
+  else{
+      res.status(200).send(validate)
+      return true;
+  }
 }
 
 export const colaboradorLogin: RequestHandler  = async (req, res) => {
-  const { matricula, senha } = req.body;
-  try {
-    const colaboradorAlreadyExists = await client.colaborador.findFirst({
-      where: {cpf: matricula}
-    })
-    if (!colaboradorAlreadyExists) {
-      return res.status(400).json({message: 'Usuário ou senha incorretos.'})
-    }
-    const passwordMatches = await compare(senha, colaboradorAlreadyExists.senha);
-
-    if(!passwordMatches) {
-      return res.status(400).json({message: 'Usuário ou senha incorretos.'})
-    }
-
-    res.status(200).json({user: {role: colaboradorAlreadyExists.role, nome: colaboradorAlreadyExists.nome}})
-  }catch(err) {
-    res.status(500).json({message: 'Houve um erro ao tentar logar, tente novamente mais tarde.'})
+    const {
+      matricula,
+      senha
+  } = req.body;
+  console.log("entuenticando");
+  const colaborador = new Colaborador(matricula, senha)
+  const validate = await Authenticator.authenticateAluno(colaborador)
+  if (validate["valid"] != true )
+  {
+      res.status(403).send(validate);
+      return false
+  }
+  else{
+      res.status(200).send(validate)
+      return true;
   }
 }

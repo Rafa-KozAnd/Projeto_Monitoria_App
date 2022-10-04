10e5cdb4af6e4740f748f810de0903e449fc0365
haissam.fawaz <haissam.fawaz@trocosimples.combr>
2022-10-04 02:31:41 +0000

    fix authorization

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -3,7 +3,7 @@ import { RequestHandler } from "express";
 import { verify } from "jsonwebtoken";
 import { client } from '../../prisma/client'
 
-export const authenticate: RequestHandler = async (req, res, next, role) => {
+export const authenticate: RequestHandler = async (req, res, next) => {
   const { authorization } = await req.headers;
   const { user_id } = await req.body;
   console.log(authorization);
@@ -13,19 +13,19 @@ export const authenticate: RequestHandler = async (req, res, next, role) => {
   const [, token] = await authorization.split(".");
   console.log(token);
   try {
-    decoded = verify(authorization, `${process.env.SECRETTOKEN}`)
+    const decoded = verify(authorization, `${process.env.SECRETTOKEN}`)
     if ( decoded.user_id != user_id )
     {
       return res.status(401).send("usuario não autorizado para realizar esta ação!");
     }
     // TODO :  Verificar como iremos separar os professores dos alunos
-    const aluno = await client.aluno.findFirst({
-      where:  {matricula : user_id }
-    });
-    if ( aluno.role != role )
-    {
-      return res.status(401).send("Usuario não autorizado para realizar esta ação!");
-    }
+    // const aluno = await client.aluno.findFirst({
+    //   where:  {matricula : user_id }
+    // });
+    // if ( aluno.role != role )
+    // {
+    //   return res.status(401).send("Usuario não autorizado para realizar esta ação!");
+    // }
     return next();
   } catch(err) {
     return res.status(401).json({ message: "Sessão expirada, realize login novamente na plataforma." });
